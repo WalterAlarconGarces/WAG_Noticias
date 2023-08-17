b8f3bff5666a9f5859dc7b8ce77d9a32b30663b5
Walter Alarcon Garces <walarcon@gmail.com>
2023-08-16 20:10:16 -0400

    setup de mail recovery password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,5 +67,14 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :user_name => '0966f55a483b7c',
+  :password => '9cebca7d8e87a4',
+  :address => 'sandbox.smtp.mailtrap.io',
+  :host => 'sandbox.smtp.mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5
+}
 end
